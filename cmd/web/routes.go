95a34b25c82c32d72936530c8df1c07b20e60713
raghu111291 <raghu.karri@gmail.com>
2022-04-24 13:26:49 +0530

    second Commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/raghu/bookings/pckgs/config"
-	"github.com/raghu/bookings/pckgs/handlers"
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"net/http"
+	"github.com/raghu/bookings/pckgs/config"
+	"github.com/raghu/bookings/pckgs/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -17,6 +18,8 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 
 	return mux
 }
